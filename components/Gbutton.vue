0cc389be4709b2629c5055588cfa5f857e3dc49f
BulyginNikita <63917819+NikBulygin@users.noreply.github.com>
2025-04-23 23:01:10 +0600

    Make some style magic (#1)
    Co-authored-by: abrakadabra <abrakadabra@mail.com>

--- components/Gbutton.vue
+++ components/Gbutton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-      class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+      class="bg-purple-700 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded"
     >
       Нажми меня
     </button>
